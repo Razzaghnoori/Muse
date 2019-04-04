235ee0bcd2df19e661ab73c73a90a92bf6885b8a
mohammad <razzaghnoori@gmail.com>
2019-04-05 02:13:29 +0430

    Playing with the data_shape of conditions

--- src/train.py
+++ src/train.py
@@ -109,7 +109,7 @@ def load_training_data(params, config):
     dataset = get_dataset(
         data, labels, config['batch_size'], params['data_shape'],
         config['use_random_transpose'], config['n_jobs'], 
-        labels_shape=(config['batch_size'], dirohe.dim))
+        labels_shape=(dirohe.dim,))
 
     # Create iterator
     if params['is_conditional']:
